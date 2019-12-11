001432741fbb570c8e375cb19380637207717008
favorable-device-5158 <blee2125@gmail.com>
2019-12-11 22:17:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,9 @@ class Scraper
   def get_page
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
+    def get_courses
+      self.get_page.css(".post")
+    end
     # doc.css(".post").each do |post|
     #   course = Course.new
     #   course.title = post.css("h2").text
